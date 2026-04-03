d7ec0fdff5ab9039540ff077b1477a7210b6378a
Rafael Pereira de Souza Castro <rcastro2@byupathway.edu>
2026-04-03 11:09:11 -0300

    Add MongoDB integration and dotenv configuration for environment variables

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const mongodb = require("./data/database");
 
 const port = process.env.PORT || 3000;
 
@@ -7,4 +8,15 @@ app.use('/', require('./routes'));
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
+});
+
+
+mongodb.initDb((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port, () => {
+      console.log(`Database is listening and node is running on port ${port}`);
+    });
+  }
 });
